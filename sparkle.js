3612ea810dfe3a91221ab96a939e5736a99b6f03
mlmargaretli6@gmail.com <>
2021-07-28 13:55:57 -0700

    added hq version of song

--- sparkle.js
+++ sparkle.js
@@ -1,19 +1,19 @@
 class Sparkle {
-  
+
   constructor(){
     this.x = random(0, width);
     this.y = random(height*0.1, height*0.9);
     this.r = random(6, 15);
     this.s = random(5, 12);
   }
-  
+
   display(){
     //light colored circles
     push();
     fill(random(120, 255), random(120, 255), random(120, 255));
     circle(this.x, this.y, this.r);
     pop();
-    
+
     //light colored squares
     push();
     rectMode(CENTER);
@@ -21,7 +21,7 @@ class Sparkle {
     fill(random(120, 255), random(120, 255), random(120, 255));
     circle(this.x, this.y, this.s);
     pop();
-    
+
     //red/bright pink squares
     push();
     rectMode(CENTER);
@@ -29,21 +29,21 @@ class Sparkle {
     fill(random(140, 255), random(135), random(135));
     square(this.x, this.y, this.s);
     pop();
-    
+
     //red circles
     push();
     translate(width*0.3, 0);
     fill(random(140, 255), random(135), random(135));
     circle(this.x, this.y, this.r);
     pop();
-    
+
     //silver circles
     push();
     translate(-width*0.2, 0);
     fill(random(120, 255));
     circle(this.x, this.y, this.r);
     pop();
-    
+
     //silver squares
     push();
     rectMode(CENTER);
