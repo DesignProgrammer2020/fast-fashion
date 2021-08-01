30c21bec2c30cea3b8e9b0083d26f7dd453097b2
mlmargaretli6@gmail.com <>
2021-08-01 16:49:44 -0700

    added some diamonds

--- sparkle.js
+++ sparkle.js
@@ -5,6 +5,7 @@ class Sparkle {
     this.y = random(height*0.1, height*0.9);
     this.r = random(6, 15);
     this.s = random(5, 12);
+    angleMode(DEGREES);
   }
 
   display(){
@@ -19,7 +20,7 @@ class Sparkle {
     rectMode(CENTER);
     translate(-width*0.3, height*0.15)
     fill(random(120, 255), random(120, 255), random(120, 255));
-    circle(this.x, this.y, this.s);
+    square(this.x, this.y, this.s);
     pop();
 
     //red/bright pink squares
@@ -48,6 +49,25 @@ class Sparkle {
     push();
     rectMode(CENTER);
     translate(-width*0.1, 0);
+    fill(random(120, 255));
+    square(this.x, this.y, this.s);
+    pop();
+
+    //light colored diamonds
+    push();
+    rectMode(CENTER);
+    // rotate(45);
+    translate(width*0.1, -height*0.2)
+    rotate(45);
+    fill(random(120, 255), random(120, 255), random(120, 255));
+    square(this.x, this.y, this.s);
+    pop();
+
+    //silver squares
+    push();
+    rectMode(CENTER);
+    translate(width*0.2, height*0.3);
+    rotate(45);
     fill(random(120, 255));
     square(this.x, this.y, this.s);
     pop();
